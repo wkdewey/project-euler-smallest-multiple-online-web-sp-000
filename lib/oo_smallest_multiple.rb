ca145380487d533db33c66b45f6608d59e258e34
wkdewey <wkdewey@gmail.com>
2020-12-10 20:28:20 -0500

    commit tree b0ebc50a2b3e56687061a0b243d747dcafdfb757

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,40 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+  @@primes = [2, 3]
+  def initialize(number)
+    @number = number
+    @prime_factors = []
+  end
+
+  def self.is_prime?(number)
+    prime = true
+    if !(@@primes.include?(number))
+      squareRoot = Math.sqrt(number).floor
+      for i in 2..squareRoot
+        if ((number % i) == 0) && is_prime?(i)
+          prime = false
+        end
+      end
+    end
+    if prime == true && !(@@primes.include?(number))
+      @@primes << number
+    end
+    prime
+  end
+
+  def lcm
+    (2..@number).each do |i|
+      if self.class.is_prime?(i)
+        @prime_factors << i
+      end
+    end
+    result = 1
+    puts(@prime_factors)
+    @prime_factors.each do |factor|
+      result *= factor
+      puts result
+    end
+    result
+  end
+
+end
